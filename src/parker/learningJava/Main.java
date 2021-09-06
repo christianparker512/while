bc2ffc995f2266ef5fa248dfbe9300d5290f594b
Christian Parker <christianparker512@gmail.com>
2021-09-05 20:00:06 -0500

    for loop added

--- src/parker/learningJava/Main.java
+++ src/parker/learningJava/Main.java
@@ -4,9 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 	    int count = 0;
-	    while(count !=5){
+	    while(count !=6){
             System.out.println("Count value is " + count);
             count ++;
+        }
+	    for(count = 1; count != 6; count++){
+            System.out.println("count value is " + count);
         }
     }
 }
