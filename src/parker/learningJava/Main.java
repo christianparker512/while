4ed63b18f9f43cca556d4e41a80b8e21fef60ce2
Christian Parker <christianparker512@gmail.com>
2021-09-05 20:02:47 -0500

    while if

--- src/parker/learningJava/Main.java
+++ src/parker/learningJava/Main.java
@@ -4,7 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 	    int count = 0;
-	    while(count !=6){
+	    while(true){
+            if(count ==6) {
+                break;
+            }
             System.out.println("Count value is " + count);
             count ++;
         }
