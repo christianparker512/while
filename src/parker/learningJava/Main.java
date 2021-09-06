b9fef192b47d94f5b1b70e40a871fba031eaf250
Christian Parker <christianparker512@gmail.com>
2021-09-05 19:56:46 -0500

    Initial commit

---
+++ src/parker/learningJava/Main.java
@@ -0,0 +1,12 @@
+package parker.learningJava;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int count = 0;
+	    while(count !=5){
+            System.out.println("Count value is " + count);
+            count ++;
+        }
+    }
+}
